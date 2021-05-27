2af990b3ea165abce4d2bf5eb0c6e914470d2a83
shuntacreative <shuntacreative@gmail.com>
2021-05-27 17:48:17 +0900

    ユーザー修正３

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -81,12 +81,6 @@ RSpec.describe User, type: :model do
        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password には英字と数字の両方を含めて設定してください")
       end 
 
-      it 'passwordが半角数字のみでは登録できない' do 
-       @user.password = 'qwerty'
-       @user.valid?
-       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password には英字と数字の両方を含めて設定してください")
-      end 
-
       it 'passwordが全角では登録できない' do 
        @user.password = 'kbc１２３'
        @user.valid?
