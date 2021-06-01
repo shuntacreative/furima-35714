96232ddc1f12064db5b4cc441a6d45d785f30d59
shuntacreative <shuntacreative@gmail.com>
2021-06-01 11:09:40 +0900

    商品一覧　修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
    
   def index
     @products = Product.order("created_at DESC")
