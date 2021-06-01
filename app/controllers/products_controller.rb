cefa01decf174ba0b260b4f301c591e34793e569
shuntacreative <shuntacreative@gmail.com>
2021-06-01 16:38:32 +0900

    修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update]
    
   def index
     @products = Product.order("created_at DESC")
@@ -51,7 +51,7 @@ class ProductsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && @product.user_id == current_user.id
+    unless user_signed_in?
         redirect_to root_path
      end
 end
