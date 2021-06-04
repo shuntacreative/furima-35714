11c60f1149f8ce5688acce4b2edbac15781edfe6
shuntacreative <shuntacreative@gmail.com>
2021-06-04 16:38:34 +0900

    修正

--- app/models/purchase_user_address.rb
+++ app/models/purchase_user_address.rb
@@ -22,8 +22,3 @@ include ActiveModel::Model
     end
 end
 
-#ickname: nickname, email: email, password: password, first_name: first_name_kanji, family_name: last_name_kanji, first_name_kana: first_name_kana, family_name_kana: family_name_kana, birthday: birthday
-#(name: name, flat_number: flat_number, post_code: post_code, description: description, category_id: category.id, status_id: status.id, shipping_id: shipping.id, prefecture_id: prefecture.id, send_day_id: send_day.id, price: price, user_id: user.id)
-
- #@purchase_user =  PurchaseUser.create(user_id: user_id, product_id: product_id)
- #Address.create(flat_number: flat_number, post_code: post_code, prefecture_id: prefecture.id, phone_number: phone_number, city: city, purchase_user_id: purchase_user.id)
